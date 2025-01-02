1af265c8510d941525a1776c73bdb2a9c51c3696
swartalf <ygavrilenko@gmail.com>
2025-01-02 18:51:55 +0200

    refactored

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,18 @@
 class k3s::service (
 
 ) inherits k3s {
-
   exec { 'k3s-systemd-reload':
     path        => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
     command     => 'systemctl daemon-reload > /dev/null',
     refreshonly => true,
   }
 
-  service{"k3s":
-    ensure  => $service_ensure,
-    enable  => $service_enable,
+  service { 'k3s':
+    ensure  => $k3s::service_ensure,
+    enable  => $k3s::service_enable,
     require => [
-      Exec['k3s-systemd-reload'], File['/etc/systemd/system/k3s.service']
+      Exec['k3s-systemd-reload'],
+      File['/etc/systemd/system/k3s.service']
     ],
   }
 }
